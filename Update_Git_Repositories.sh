76a684db2e386b0e1e22f2ff4c82c51173e1bff4
B0rk <26283277+B0rk@users.noreply.github.com>
2023-06-11 15:11:51 -0600

    Update GitRepo Update
    Revamped the script.

--- Update_Git_Repositories.sh
+++ Update_Git_Repositories.sh
@@ -5,9 +5,10 @@ cd ~/Tools
 #Perform for loop to change into each directory and run a forced git pull on all repository folders.
 for n in $(ls -1)
 do
-	cd $n
-	echo "[+]Updating ${n}"
-	git reset --hard
-	git pull
-	cd ..
+	cd ~/Tools/$n
+	if [ -d ".git" ]; then	
+		echo "[+]Updating ${n}"
+		git reset --hard &> /dev/null
+		git pull
+	fi
 done
